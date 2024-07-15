25f6b0beaf6c19d0b2966f33895207c126077316
SZNASKME <suphawit.s@askme.co.th>
2024-07-15 16:56:19 +0700

    update

--- CompareDataExcelAPI/processGetData.py
+++ CompareDataExcelAPI/processGetData.py
@@ -1,22 +1,9 @@
+####################    API    ########################################
 import requests
 import http
 
-LIST_TASK_URI = "http://172.16.1.85:8080/uc/resources/task/list"
-LIST_TASK_ADV_URI = "http://172.16.1.85:8080/uc/resources/task/listadv"
+from compareDataApp import LIST_TASK_URI, LIST_TASK_ADV_URI, SHEET_NAME, task_configs, task_adv_configs, auth
 
-task_configs = {
-    'name': '*',
-    'type':  'Timer'
-}
-
-task_adv_configs = {
-    'taskname': '*',
-    'type': 'Timer',
-    'updatedTime': '-10d',
-}
-
-
-auth = ('ops.admin','p@ssw0rd')
 
 def createURI(uri, configs):
     uri += "?"
@@ -25,8 +12,6 @@ def createURI(uri, configs):
         if key != list(configs.keys())[-1]:
             uri += "&"
     return uri
-
-
 
 
 def getListTask():
@@ -39,7 +24,7 @@ def getListTaskAdvanced():
     response = requests.get(url=uri, auth=auth, headers={'Accept': 'application/json'})
     return response
 
-def getData(mode = 0):
+def getDataAPI(mode = 0):
     
     if mode == 0:
         response = getListTask()
@@ -49,6 +34,55 @@ def getData(mode = 0):
     status = http.HTTPStatus(response.status_code)
     print(f"{response.status_code} - {status.phrase}: {status.description}")
     if response.status_code == 200:
-        return response.text
+        size = len(response.content)
+        print("Size of API data: ", size / 1024, "KB")
+        return response.json()
     else:
         return None
+    
+    
+####################    Excel    ########################################
+import pandas as pd
+
+def inputMethod():
+    def get_path_and_sheetname(prompt):
+        print(prompt)
+        input_value = input().strip()
+        if '/' in input_value:
+            path, sheetname = input_value.split('/', 1)
+        else:
+            path, sheetname = input_value, None
+        return path, sheetname
+    
+    path, sheetname = get_path_and_sheetname("Enter PATH of the file and sheetname [pathfile/sheetname]: ")
+    
+    return {'path': path, 'sheetname': sheetname }
+    
+
+def readExcelMultipleSheet(pathfile, sheetname=None):
+    try:
+        return pd.read_excel(pathfile, sheet_name=sheetname, engine='openpyxl')
+    except Exception as e:
+        print(f"Error reading {pathfile}: {e}")
+        return None
+
+def selectSheet(dfs, sheetname):
+    if sheetname == None:
+        return dfs[SHEET_NAME]
+    else:
+        return dfs[sheetname]
+
+
+def getDataExcel():
+    excel_configs = inputMethod()
+    print('Reading excel file...')
+    dfs = readExcelMultipleSheet(excel_configs['path'], excel_configs['sheetname'])
+    if dfs is None:
+        print('Error reading excel file')
+        return None
+    
+    print('selecting sheet . . .')
+    dfs_sheet = selectSheet(dfs, excel_configs['sheetname'])
+    
+    print('Excel data read successfully')
+    return dfs_sheet
---
+++ GetDataAPI/API_Data_to_Excel.py
@@ -0,0 +1,86 @@
+import requests
+import http
+import pandas as pd
+
+LIST_TASK_URI = "http://172.16.1.85:8080/uc/resources/task/list"
+LIST_TASK_ADV_URI = "http://172.16.1.85:8080/uc/resources/task/listadv"
+
+OUTPUT_FILE = 'Excel_API_Difference.xlsx'
+
+task_configs = {
+    'name': '*',
+    'type':  'Universal',
+}
+
+task_adv_configs = {
+    'taskname': '*',
+    'type': 'Timer',
+    'updatedTime': '-10d',
+}
+
+auth = ('ops.admin','p@ssw0rd')
+
+new_order = ['name', 'type', 'description','sysId','version']
+
+def createURI(uri, configs):
+    uri += "?"
+    for key, value in configs.items():
+        uri += f"{key}={value}"
+        if key != list(configs.keys())[-1]:
+            uri += "&"
+    return uri
+
+
+def getListTask():
+    response = requests.post(url=LIST_TASK_URI, json=task_configs, auth=auth, headers={'Accept': 'application/json'})
+    return response
+
+def getListTaskAdvanced():
+    uri = createURI(LIST_TASK_ADV_URI, task_adv_configs)
+    print(uri)
+    response = requests.get(url=uri, auth=auth, headers={'Accept': 'application/json'})
+    return response
+
+def getDataAPI(mode = 0):
+    
+    if mode == 0:
+        response = getListTask()
+    else:
+        response = getListTaskAdvanced()
+        
+    status = http.HTTPStatus(response.status_code)
+    print(f"{response.status_code} - {status.phrase}: {status.description}")
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+def createDataFrame(API_data):
+    if API_data:
+        columns = list(API_data[0].keys())
+        try:
+            return pd.DataFrame(API_data, columns=columns)
+        except Exception as e:
+            print(f"Error converting JSON to DataFrame: {e}")
+            return None
+    else:
+        return None 
+    
+def createExcel(API_data, outputfile):
+    try:
+        with pd.ExcelWriter(outputfile) as writer:
+            API_data.to_excel(writer, sheet_name='Sheet', index=False)
+
+        print("Difference file created successfully")
+    except Exception as e:
+        print(f"Error creating {outputfile}: {e}")
+        
+        
+def main():
+    APIdata = getDataAPI()
+    df = createDataFrame(APIdata)
+    df = df[new_order]
+    createExcel(df, OUTPUT_FILE)
+    
+if __name__ == "__main__":
+    main()
