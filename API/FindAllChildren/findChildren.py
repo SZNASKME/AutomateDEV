d4f7e59ad32b7a2f73265f8fe51fca954ac5b331
SZNASKME <suphawit.s@askme.co.th>
2024-08-08 15:56:23 +0700

    update

--- API/FindAllChildren/findChildren.py
+++ API/FindAllChildren/findChildren.py
@@ -9,8 +9,14 @@ TASK_URI = "http://172.16.1.86:8080/uc/resources/task"
 
 
 TASK_NAME = "MRM_MTHLY_B"
+CHILDREN_FIELD = "Children"
+CHILD_TYPE = "Child Type"
+CHILD_LEVEL = "Child Level"
+NEXT_NODE = "Next Node"
+
 
 auth = HTTPBasicAuth('ops.admin','p@ssw0rd')
+
 
 
 task_configs_temp = {
@@ -36,26 +42,27 @@ def getTaskAPI(task_configs):
     return response
 
 
-############################################################################################################
+#########################################     find children    ################################################
 
-def findChildren(task_name, next_node = []):
-    children = {"Children": OrderedDict(), "Next Node": None}
+def findChildren(task_name, next_node = [], level = 1):
+    children = {CHILD_TYPE: None, CHILD_LEVEL: level, CHILDREN_FIELD: OrderedDict(), NEXT_NODE: None}
     task_configs = task_configs_temp.copy()
     task_configs['taskname'] = task_name
     response = getTaskAPI(task_configs)
     status = http.HTTPStatus(response.status_code)
     if response.status_code == 200:
         task_data = json.loads(response.text)
+        children[CHILD_TYPE] = task_data['type']
         if task_data['type'] == "taskWorkflow":
             for child in task_data['workflowVertices']:
                 child_name = child['task']['value']
                 child_next_node = findNextNode(child_name, task_data['workflowEdges'])
-                children["Children"][child_name] = findChildren(child_name, child_next_node)
+                children["Children"][child_name] = findChildren(child_name, child_next_node, level + 1)
         if len(next_node) > 0:
-            children["Next Node"] = next_node
+            children[NEXT_NODE] = next_node
         else:
-            children["Next Node"] = []
-    
+            children[NEXT_NODE] = []
+        
     #print(f"{response.status_code} - {status.phrase}: {status.description}")
 
     return children
@@ -67,6 +74,12 @@ def findNextNode(task_name, workflowEdges):
             next_node.append(edge['targetId']['taskName'])
     return next_node
 
+def countChildren(children_dict):
+    count = 0
+    for child_name, child_data in children_dict["Children"].items():
+        count += 1  # Count this child
+        count += countChildren(child_data)  # Recursively count the child's children
+    return count
 
 ############################################       tree         ############################################
 
@@ -76,8 +89,8 @@ from tkinter import ttk
 def insert_items(parent, children, tree):
     for key, value in children.items():
         item_id = tree.insert(parent, 'end', text=key)
-        insert_items(item_id, value['Children'])
-        for next_node in value.get('Next Node', []):
+        insert_items(item_id, value[CHILDREN_FIELD], tree)
+        for next_node in value.get(NEXT_NODE, []):
             tree.insert(item_id, 'end', text=next_node)
 
 def createTreeView(data):
@@ -90,7 +103,7 @@ def createTreeView(data):
 
     # Insert the root item
     root_item = tree.insert('', 'end', text='ROOT')
-    insert_items(root_item, data['Children'], tree)
+    insert_items(root_item, data[CHILDREN_FIELD], tree)
 
     # Pack the Treeview widget
     tree.pack(fill='both', expand=True)
@@ -106,11 +119,11 @@ import matplotlib.pyplot as plt
 
 
 def add_nodes_and_edges(graph, parent, data):
-    for node, info in data['Children'].items():
+    for node, info in data[CHILDREN_FIELD].items():
         graph.add_node(node)
         graph.add_edge(parent, node)
         add_nodes_and_edges(graph, node, info)
-        for next_node in info.get("Next Node", []):
+        for next_node in info.get(NEXT_NODE, []):
             graph.add_node(next_node)
             graph.add_edge(node, next_node)
 
@@ -139,7 +152,8 @@ def main():
     children = findChildren(TASK_NAME)
     print("Showing the children of the task in a tree view")
     print(json.dumps(children, indent=10))
-    
+    total_children = countChildren(children)
+    print(f"Total children: {total_children}")
     #visualizeWorkflow(children)
     createTreeView(children)
 
